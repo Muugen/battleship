3b13e8b8fdc28e7ddfc1f816f227c2e78ad6de91
tsuplige <thomassupligeon@gmail.com>
2019-11-08 03:17:20 +0100

    message pour vaisseau et autres

---
+++ battleship.php
@@ -0,0 +1,12 @@
+<?php
+echo "veuillez entrez les coordonné de vos vaisseau :\n";
+echo "--- un porte_avion de 5 cases ---\n";
+echo "--- un croiseur de 4 cases ---\n";
+echo "--- un contre_torpilleur de 3 cases ---\n";
+echo "--- un sous_marin de 3 cases ---\n";
+echo "--- un torpilleur de 2 cases ---\n";
+$porte_avion = "PAPAP";
+$croiseur = "CCCC";
+$contre_torpilleur = "CTC";
+$sous_marin = "SMM";
+$torpilleur = "TT";
